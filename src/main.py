20a27715e27264c8c8a4bb4d0e548d8207ff6074
carloscortes2000 <Carlos_Daniel_Cortes_Jimenez@ciencias.unam.mx>
2024-05-21 16:08:28 -0600

    actualizado etiquetado de categorias

--- src/main.py
+++ src/main.py
@@ -4,12 +4,19 @@ from entrenaRNN import entrenar_rnn, hacer_predicciones
 def main():
     archivo_entrada = 'poemas.json'
     archivo_salida = 'poemas_limpios.json'
-    modelo, tokenizer = entrenar_rnn(archivo_salida)
+    limpiar_poemas(archivo_entrada, archivo_salida)
+
+    modelo, tokenizer, encoder = entrenar_rnn(archivo_salida)
+
+    # Imprimir categorias
+    categorias = encoder.classes_
+    print(f'Available Categories: {categorias}')
 
     # Ejemplo de hacer predicciones
-    nuevo_poema = "Una vaga inquietud un misterioso temor como un feliz presentimiento un íntimo y recóndito tormento una pena que acaba en alborozo el sofocante nudo de un sollozo perenne en la garganta el sentimiento de un dolor que se acerca el pensamiento lleno de luz de júbilo de gozo una contradicción honda y obscura que me llena la vida de amargura que mata toda luz y toda idea que turba toda paz toda alegría pero Señor que sabes mi agonía si todo esto es amor bendito sea"
-    prediccion = hacer_predicciones(modelo, nuevo_poema, tokenizer)
-    print(f'Predicción: {prediccion}')
+    nuevo_poema = "llueve en mi alma una tormenta sin fin lagrimas que caen sin poder resistir el viento susurra historias de dolor y en mi corazon solo queda el rencor las estrellas se esconden la luna no brilla la noche es eterna sin ninguna maravilla en cada rincon se esconde un recuerdo de tiempos felices que ahora pierdo el sol no amanece el dia no llega mi vida es un eco de una voz ciega caminos vacios sin rumbo ni guia perdido en la sombra sin alegria llueve en mi alma una tormenta sin fin mi corazon roto no puede seguir el pasado me ata no hay futuro que vea mi tristeza infinita mi alma lacera"
+    modelo_rnn, tokenizer, encoder = entrenar_rnn('poemas_limpios.json')
+    categoria_predicha = hacer_predicciones(modelo_rnn, nuevo_poema, tokenizer, encoder)
+    print(f'La categoría predicha para el poema es: {categoria_predicha}')
 
 if __name__ == "__main__":
     main()
